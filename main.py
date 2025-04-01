cf1fc1c875b9c3741a902741856c523d7ec2f97a
Fabian Krieg <fabian.krieg@tu-ilmenau.de>
2025-04-01 19:56:52 +0200

    tidy up - this version actually plays 3 sounds!

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
 import time
-
 sys.path.insert(0, "bindings")  # Ensure Python can find the module
 import audio_engine
 
@@ -12,5 +11,7 @@ engine.setFrequency(880.0)  # A4 -> A5
 time.sleep(2)
 
 engine.setFrequency(440.0)  # Back to A4
+time.sleep(2)
+engine.setFrequency(880.0)  # A4 -> A5
 time.sleep(2)
 
