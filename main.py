433cad8efd413cb6c084c38d4cd7aefe5b2b90ba
Fabian Krieg <fabian.krieg@tu-ilmenau.de>
2025-04-01 19:54:32 +0200

    remove obsolete stuff

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import sys
 import time
-import random
 
 sys.path.insert(0, "bindings")  # Ensure Python can find the module
 import audio_engine
 
-import time
 
 engine = audio_engine.AudioEngine()
 
