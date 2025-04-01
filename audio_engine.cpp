c2dbe386ecf47d623f1fd5b4d7b03cb4d927760c
Fabian Krieg <fabian.krieg@tu-ilmenau.de>
2025-04-01 13:15:21 +0200

    format code and carry sample rate internally

--- audio_engine.cpp
+++ audio_engine.cpp
@@ -1,4 +1,4 @@
-#include <juce_audio_basics/juce_audio_basics.h>
+#include <juce_audio_utils/juce_audio_utils.h>  
 #include <juce_audio_devices/juce_audio_devices.h>
 #include <juce_audio_basics/juce_audio_basics.h>
 #include <pybind11/pybind11.h>
@@ -33,37 +33,44 @@ class AudioEngine : public juce::AudioAppComponent
 {
 public:
     AudioEngine() {
-        setAudioChannels(0,2);
+        setAudioChannels(0, 2);
     }
+
     ~AudioEngine() {
         shutdownAudio();
     }
-    
+
     void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override {
-        osc.setFrequency(frequency, sampleRate);
+        this->currentSampleRate = sampleRate;  // Store sample rate for later use
+        osc.setFrequency(frequency, currentSampleRate);
     }
-    
+
     void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override {
-        for(int channel = 0; channel < bufferToFill.buffer->getNumChannels(); ++channel)
-        {
+        for (int channel = 0; channel < bufferToFill.buffer->getNumChannels(); ++channel) {
             auto* buffer = bufferToFill.buffer->getWritePointer(channel, bufferToFill.startSample);
-            for(int sample = 0; sample < bufferToFill.numSamples; ++sample)
+            for (int sample = 0; sample < bufferToFill.numSamples; ++sample)
                 buffer[sample] = osc.getNextSample();
         }
     }
 
     void releaseResources() override {}
 
-    void setFrequency(float newFrequency)
-    {
+    void setFrequency(float newFrequency) {
         frequency = newFrequency;
-        osc.setFrequency(frequency, getSampleRate());
+        if (currentSampleRate > 0)  // Ensure we have a valid sample rate
+            osc.setFrequency(frequency, currentSampleRate);
+    }
+
+    double getStoredSampleRate() const {
+        return currentSampleRate;
     }
 
 private:
     float frequency = 440.0f;
+    double currentSampleRate = 0.0;  // Store the sample rate
     SimpleSynth osc;
 };
+
 
 namespace py = pybind11;
 PYBIND11_MODULE(audio_engine, m)
