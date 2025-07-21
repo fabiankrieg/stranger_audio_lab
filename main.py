533eaac2dbcdb3347dbbddd50f4a8e52b4fefbd0
Fabian Krieg <fabian.krieg@tu-ilmenau.de>
2025-07-21 19:08:05 +0200

    register a parameters pitchBend so we can alter it from the main loop

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ sys.path.insert(0, "./bindings")
 from example_simple_multi_part_song import SimpleMultiPartSong
 from stranger_playback import StrangerPlayback
 import audio_engine
+from time import sleep
 
 # Create ControlParameters
 control_params = audio_engine.ControlParameters()
@@ -14,11 +15,21 @@ control_params = audio_engine.ControlParameters()
 bpm = 250  # Beats per minute
 song = SimpleMultiPartSong(control_params, bpm)
 
+synths = song.get_synthesizers()
+for synth_name, synth in synths.items():
+    control_params.linkParameter(synth_name, "pitchBend", "pitchBend")
+    
 # Initialize playback
 playback = StrangerPlayback(song, control_params)
 
+current_pitch_bend = 0
 # Start playback
 try:
+    control_params.updateParameter("pitchBend", current_pitch_bend)
+    current_pitch_bend += 1
+    if current_pitch_bend > 127:
+        current_pitch_bend = 0
+    print(f"Current pitch bend: {current_pitch_bend}")
     playback.start_playback()
 except KeyboardInterrupt:
     print("Playback interrupted by user.")
